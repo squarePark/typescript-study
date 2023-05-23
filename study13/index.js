2a5726790835c7fed79e80c87aebf30f5db3e2b3
jinjoo.Park <aaeey12@gmail.com>
2023-05-23 21:52:48 +0900

    naver 타입 써보기

---
+++ study13/index.js
@@ -0,0 +1,13 @@
+// function return 값에 붙일 수 있는 never type
+// 조건1 return 값이 없어야 함
+// 조건2 endpoint가 없어야 함
+function 함수13() {
+    throw new Error();
+}
+function 함수13_1() {
+    while (true) {
+        // 내 코드를 무한히 돌아감
+    }
+}
+// 실제 사용 예 naver -> void 로 대체 가능하다.
+// 간혹 등장하는 naver 쉐키

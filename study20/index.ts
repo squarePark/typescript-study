fef75c8563e2dd13046daa828dedc1074d738417
jinjoo.Park <aaeey12@gmail.com>
2023-06-04 12:05:40 +0900

    .d.ts 파일이 무엇인가

---
+++ study20/index.ts
@@ -0,0 +1,4 @@
+// d.ts 는 무엇인가
+import { Study20 } from './test';
+
+let age20: Study20;
---
+++ study20/test.d.ts
@@ -0,0 +1,8 @@
+// .d.ts는 무엇인가
+// 타입정의 보관용 파일
+// 레퍼런스용으로 d.ts 파일 쓰기
+// 자바스크립트 라이브러리들은 d.ts 를 제공해 준다.
+export type Study20 = number;
+export interface Person20 {
+  name: string;
+}
